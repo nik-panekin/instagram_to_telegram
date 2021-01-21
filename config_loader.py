43b74a75cc139fd7b5e3120689200408d6230e29
nik-panekin <nik.reflective@gmail.com>
2021-01-21 19:28:36 +0200

    Readme added

--- config_loader.py
+++ config_loader.py
@@ -1,5 +1,5 @@
-"""В данном модуле хранятся (или подгружаются извне) настройки для бота и
-выполняется инициализация подсистемы ведения журнала ошибок (logging).
+"""В данном модуле выполняется подгрузка настроек бота из конфигурационного
+файла и выполняется инициализация подсистемы ведения журнала ошибок (logging).
 """
 import os
 import logging
@@ -24,19 +24,25 @@ LOG_BACKUPS = 2
 # Название папки для сохранения временных файлов (например, загрузки)
 TEMP_FOLDER = 'tmp'
 
+# Имя конфигурационного файла
 CONFIG_NAME = 'config.ini'
 
+# Имя файла для хранения HTTP Cookies
 COOKIEJAR = 'cookies.dat'
 
+# Таймаут HTTP-запроса (секунды)
 REQUEST_TIMEOUT = 30
 
+# Задержка после отправки сообщения в Telegram (секунды)
 SEND_MESSAGE_DELAY = 2
 
+# Максимальная длина текстовой подписи к медиафайлу в Telegram
 MAX_CAPTION_LENGTH = 1024
 
+# Концовка, добавляемая к обрезанной текстовой подписи
 CAPTION_TAIL = '...'
 
-"""Далее следует настройка логгирования (журнала ошибок и уведомлений).
+"""Настройка логгирования (журнала ошибок и уведомлений).
 """
 logFormatter = logging.Formatter(fmt='[%(asctime)s] %(filename)s:%(lineno)d '
                                      '%(levelname)s - %(message)s',
@@ -61,41 +67,55 @@ if os.path.exists(LOG_FOLDER):
     fileHandler.setFormatter(logFormatter)
     rootLogger.addHandler(fileHandler)
 
-"""Подгрузка параметров из конфигурационного файла
+"""Подгрузка параметров из конфигурационного файла.
 """
 parser = ConfigParser()
-parser.read(CONFIG_NAME)
+parser.read(CONFIG_NAME, encoding='utf-8')
 
 try:
+    # API TOKEN бота (получают у @BotFather)
     BOT_TOKEN = parser.get('credentials', 'token')
+
+    # Названия аккаунтов пользователей Instagram для мониторинга
     INSTAGRAM_USER_NAMES = parser.get('general', 'usernames').split(',')
+
+    # Идентификатор канала Telegram для пересылки извлечённой информации
     TELEGRAM_CHAT_ID = parser.get('general', 'channel_id')
 except Exception as e:
     logging.error('Не удалось загрузить базовые настройки. '
                   'Работа программы завершается.')
     sys.exit()
 
+# Логин владельца аккаунта Instagram
 LOGIN = parser.get('credentials', 'login', fallback=None)
+
+# Пароль владельца аккаунта Instagram
 PASSWORD = parser.get('credentials', 'password', fallback=None)
 
+# Флаг режима ручной авторизации в Instagram
 MANUAL_AUTH = parser.get('credentials', 'manual_auth', fallback='')
 if MANUAL_AUTH.strip().lower() in ['true', '1']:
     MANUAL_AUTH = True
 else:
     MANUAL_AUTH = False
 
+# Добавлять ли ссылку на запись Instagram в конец сообщения
 INCLUDE_LINK = parser.get('general', 'include_link', fallback='')
 if INCLUDE_LINK.strip().lower() in ['true', '1']:
     INCLUDE_LINK = True
 else:
     INCLUDE_LINK = False
 
+# Максимальное количество единиц медиа для каждого аккаунта Instagram,
+# извлекаемое за один цикл работы скрипта
 MEDIA_LIMIT = parser.get('general', 'limit', fallback='')
 if MEDIA_LIMIT.strip().isdigit():
     MEDIA_LIMIT = int(MEDIA_LIMIT.strip())
 else:
     MEDIA_LIMIT = 5
 
+# Периодичность запуска скрейпинга Instagram при работе в режиме бесконечного
+# цикла (секунды)
 SCRAPE_PERIOD = parser.get('general', 'period', fallback='')
 if SCRAPE_PERIOD.strip().isdigit():
     SCRAPE_PERIOD = int(SCRAPE_PERIOD.strip())
